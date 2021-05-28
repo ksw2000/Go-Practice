d373956f7a8abf946b9f2ad2ec6b1eaf635ca489
liao2000 <liao2000@livemail.tw>
2021-05-29 02:03:15 +0800

    Update 00-03

--- basic/03-For-Loop.go
+++ basic/03-For-Loop.go
@@ -1,29 +1,28 @@
 package main
+
 import "fmt"
-func main(){
-    for i:=2; i<10 ;i++ {
-        for j:=2; j<10 ; j++ {
-            fmt.Printf("%dx%d=%-2d ",i,j,i*j)
-        }
-        fmt.Printf("\n")
-    }
 
-    for i:=1; i<=19 ;i++ {
-        fmt.Println("2x",i,"=",2*i)
-    }
+func main() {
+	for i := 2; i < 10; i++ {
+		for j := 2; j < 10; j++ {
+			fmt.Printf("%dx%d=%-2d ", i, j, i*j)
+		}
+		fmt.Printf("\n")
+	}
 
-    //GOlang use "for" instead of "while"
-    /*
-    C:
-        int i=0;
-        while(i<10){
-            printf("%d ",i);
-            i++;
-        }
-    */
-    i:=0;
-    for i<10 {
-        fmt.Printf("%d ",i)
-        i++
-    }
+	i := 0
+	for i < 10 { // while(i < 10) in C
+		fmt.Printf("%d ", i)
+		i++
+	}
+
+outer:
+	for i := 0; i <= 10; i++ {
+		for j := 0; j <= 10; j++ {
+			if i+j == 10 {
+				break outer
+			}
+			fmt.Printf("(%d, %d)\n", i, j)
+		}
+	}
 }
