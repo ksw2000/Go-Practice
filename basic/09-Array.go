a5567e582366faad0bde23e41a181a6905f75326
liao2000 <liao2000@livemail.tw>
2021-06-26 02:04:19 +0800

    Update 08-10

--- basic/09-Array.go
+++ basic/09-Array.go
@@ -1,21 +1,23 @@
 package main
+
 import "fmt"
-func main(){
-    // 類型標記法
-    var array1 [3]string
-    array1[0]="C"
-    array1[1]="C++"
-    array1[2]="GO"
-    fmt.Println(array1)
 
-    // 值標記法
-    // := 宣告 + 賦值 限函數內
-    array2 := [3]string{"C","C++","GO"}
-    fmt.Println(array2)
+func main() {
+	// 類型標記法
+	var array1 [3]string
+	array1[0] = "C"
+	array1[1] = "C++"
+	array1[2] = "GO"
+	fmt.Println(array1)
 
-    array3 := [...]string{"C","C++","GO"}
-    fmt.Println(array3)
+	// 值標記法
+	// := 宣告 + 賦值 限函數內
+	array2 := [3]string{"C", "C++", "GO"}
+	fmt.Println(array2)
 
-    array4 := [6]string{0:"C", 1:"C++", 2:"GO", 3:"PHP"}
-    fmt.Println(array4)
+	array3 := [...]string{"C", "C++", "GO"}
+	fmt.Println(array3)
+
+	array4 := [6]string{0: "C", 1: "C++", 2: "GO", 3: "PHP"}
+	fmt.Println(array4)
 }
--- basic/10-Slice.go
+++ basic/10-Slice.go
@@ -1,30 +1,33 @@
 package main
+
 import "fmt"
-func main(){
-    //切片與陣列最大的差別在於切片不必指定長度
-    slice1 := []string{"C","C++","GO"}
-    fmt.Println(slice1[0:1])
-    /*
-        切片底層的資料結構
-        ptr *Elem   儲存其底層陣列的指標
-        int len     切片長度
-        int cap     切片容量。一個切片值的容量「不是」其底層陣列的長度
 
-        利用 append(oldslice,element,[elements...]) 擴充slice
-        slice的容量(cap)是不能改的，利用append方法可以重新建立slice
-        以達擴充效果
-    */
-    slice2 := append(slice1,"python","Java")
-    fmt.Println(slice2)
+func main() {
+	//切片與陣列最大的差別在於切片不必指定長度
+	slice1 := []string{"C", "C++", "GO"}
+	fmt.Println(slice1[0:1])
+	/*
+	   切片底層的資料結構
+	   ptr *Elem   儲存其底層陣列的指標
+	   int len     切片長度
+	   int cap     切片容量。一個切片值的容量「不是」其底層陣列的長度
 
-    /*
-        利用 make() 進行切片的宣告
-        注意 make() 只能對「切片、字典、通道」三種型態進行宣告
-    */
-    // 利用 make 宣告 slice
-    // 型態(arg[0]):[]int
-    // 容量(arg[1]):20
-    // 長度(arg[2]):10
-    // 若忽略 arg[2] 則視 arg[2] = arg[1]
-    slice3 := make([]int, 10, 20);
+	   利用 append(oldslice,element,[elements...]) 擴充slice
+	   slice的容量(cap)是不能改的，利用append方法可以重新建立slice
+	   以達擴充效果
+	*/
+	slice2 := append(slice1, "python", "Java")
+	fmt.Println(slice2)
+
+	/*
+	   利用 make() 進行切片的宣告
+	   注意 make() 只能對「切片、字典、通道」三種型態進行宣告
+	*/
+	// 利用 make 宣告 slice
+	// 型態(arg[0]):[]int
+	// 容量(arg[1]):20
+	// 長度(arg[2]):10
+	// 若忽略 arg[2] 則視 arg[2] = arg[1]
+	slice3 := make([]int, 10, 20)
+	fmt.Println(slice3)
 }
