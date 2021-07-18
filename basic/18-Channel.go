289f8ad5d2678ab2a19456bb911f4bdc726d7b63
liao2000 <liao2000@livemail.tw>
2021-07-19 02:05:26 +0800

    Update 17-18

--- basic/18-Channel.go
+++ basic/18-Channel.go
@@ -1,50 +1,57 @@
 package main
-import "fmt"
 
-func sender1(c chan string){
-    c <- "sender1"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func sender1(c chan string) {
+	time.Sleep(1 * time.Second)
+	c <- "sender1"
 }
 
-func sender2(c chan string){
-    c <- "sender2"
+func sender2(c chan string) {
+	time.Sleep(2 * time.Second)
+	c <- "sender2"
 }
 
-func main(){
-    //https://colobu.com/2016/04/14/Golang-Channels/
-    ch1 := make(chan string)
-    ch2 := make(chan string)
+func main() {
+	// https://colobu.com/2016/04/14/Golang-Channels/
+	// declare two unbuffered channels
+	ch1 := make(chan string)
+	ch2 := make(chan string)
 
-    //select
-    go sender1(ch1)
-    go sender2(ch2)
+	// select
+	go sender1(ch1)
+	go sender2(ch2)
 
-    s,t := <- ch1, <- ch2
-    //s,t 接收到 channel 後才會往下走
+	// like .join() in java
+	// main thread will be blocked until receiving channel's vaule
+	s, t := <-ch1, <-ch2
 
-    fmt.Println(s, t)
+	fmt.Println(s, t)
 
-    //利用 range 接收不確定數量的 channel
-    n := make(chan int)
-    go func(end int, n chan int){
-        for i:=0; i<end; i++{
-            n <- i
-        }
-        close(n)
-    }(10,n)
+	// use range to get the uncertain length of values
+	n := make(chan int)
+	go func(end int, n chan int) {
+		for i := 0; i < end; i++ {
+			n <- i
+		}
+		close(n)
+	}(rand.Int()%10, n)
 
-    fmt.Println("接收")
-    //range 會一直接收 n 直到 n 被關閉為止
-    for v := range n{
-        fmt.Printf("%d ",v)
-    }
+	fmt.Println("Start receiving")
+	for v := range n {
+		fmt.Printf("%d ", v)
+	}
 
-    //Buffer
-    //https://blog.wu-boy.com/2019/04/understand-unbuffered-vs-buffered-channel-in-five-minutes/
-    //unbuffered channel 就是代表在主程式內，需要等到讀或寫都完成
-    //main 才可以完整結束 (讀跟寫 buffered channel 需要在不同的 goroutine 才不會被 block)
-
-    //buffered channel 相反，你可以一直丟資料進去 Channel 內
-    //不需要讀出來
-    //（前提是 buffered channel 空間夠大不會爆掉）
-    //所以 main 才提前結束。
+	// Buffered channel do not cause block
+	ch3 := make(chan int, 8)
+	go func() {
+		ch3 <- 8
+		time.Sleep(1 * time.Second)
+		fmt.Println("buffer channel")
+	}()
+	<-ch3
 }
