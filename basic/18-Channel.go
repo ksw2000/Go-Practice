45a3bcf47af08c8545ca56429f1d493a275bfe28
liao2000 <liao2000@livemail.tw>
2021-07-28 02:38:22 +0800

    Buffered & Unbuffered

--- basic/18-Channel.go
+++ basic/18-Channel.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"time"
 )
 
@@ -39,19 +38,54 @@ func main() {
 			n <- i
 		}
 		close(n)
-	}(rand.Int()%10, n)
+	}(10, n)
 
 	fmt.Println("Start receiving")
 	for v := range n {
 		fmt.Printf("%d ", v)
 	}
 
-	// Buffered channel do not cause block
-	ch3 := make(chan int, 8)
+	fmt.Println()
+	fmt.Println()
+
+	// Unbuffered channel causes block
+	ch3 := make(chan int)
 	go func() {
-		ch3 <- 8
-		time.Sleep(1 * time.Second)
-		fmt.Println("buffer channel")
+		ch3 <- 1
+		fmt.Println("ok1")
+		ch3 <- 2
+		fmt.Println("ok2")
 	}()
 	<-ch3
+	time.Sleep(3 * time.Second)
+	<-ch3
+
+	time.Sleep(5 * time.Second)
+	fmt.Println()
+	/*
+		ok1
+		(wait 5 second)
+		ok2
+	*/
+
+	// Buffered channel do not cause block
+	// Even if main routing have not read channel
+	// go routing will still send value to channel
+
+	ch4 := make(chan int, 5)
+	go func() {
+		ch4 <- 3
+		fmt.Println("ok3")
+		ch4 <- 4
+		fmt.Println("ok4")
+	}()
+	<-ch4
+	time.Sleep(5 * time.Second)
+	<-ch4
+
+	/*
+		ok3
+		(without waiting)
+		ok4
+	*/
 }
